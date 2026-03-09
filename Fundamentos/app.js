672614e97c6f7364ec5aad9061d0f199674d52c4
[Yurany] <yuranysanchezmar18@gmail.com>
2026-03-09 17:16:47 -0500

    Inicio de curso (variables -- depuración)

---
+++ Fundamentos/app.js
@@ -0,0 +1,39 @@
+// console.log('Hola mundo');
+
+// let a = 10;
+// var b = 10;
+// const c = 10; // valor de memoria y no se puede cambiar
+
+// cambiar el valor de la variable a y b
+// a= 20;
+// b= 20;
+
+// var forma antigua de iniciar variable 
+// let y const forma moderna de iniciar variable
+
+/* 
+ES5 soportado por la mayoria de navegadores 
+ES6 soportado por navegadores modernos 
+
+Polyfill  --- Rellena una funcion que falta para que ese navegador soporte la función*/
+
+let a = 10 ,
+    b = 10 ,
+    c = ' ZOE ' ,
+    d = ' ARY ' ,
+    y = a + b;
+
+    const saludo = c + d ;
+
+// console.log('%c Mis variables' , 'color: lightblue; font-weight: bold; font-size: 20px;' );
+// console.log( { a } );
+// console.log( { b } );
+// console.log( { c } );
+// console.log( { d } );
+// console.log(  { y } );
+
+// console.table({ a, b, c, d, y });
+
+
+// DEPURACION Y BREAKPOINTS
+
