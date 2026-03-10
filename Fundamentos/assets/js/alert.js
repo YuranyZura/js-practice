e5bee232423926e600203a88828109e85ec9fb32
[Yurany] <yuranysanchezmar18@gmail.com>
2026-03-10 11:56:23 -0500

    Ejercicios de datos primitivos

--- Fundamentos/assets/js/alert.js
+++ Fundamentos/assets/js/alert.js
@@ -1,3 +1,13 @@
 
 
 // console.log( miNombre );
+
+alert('Oi, voce é um gato!');
+
+let nombre = prompt('what is your name?');  //recibe mensaje o string
+console.log(nombre);
+console.log ('###' + nombre + '###' );
+
+// Confirmacion
+const seleccion = confirm('¿Estas seguro de que quieres continuar?'); //recibe un mensaje o string y devuelve un booleano
+console.log( seleccion );
---
+++ Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,37 @@
+
+
+let nombre = 'Niña';
+console.log ( nombre );
+
+nombre = 'Peter';
+console.log ( nombre );
+
+nombre = "Zoe";
+nombre = "Celular";
+
+// Diferencia '' y "" --- se remoienda comilla simple 
+
+console.log (typeof nombre);
+
+nombre = 123;
+console.log( typeof nombre);
+
+let esGato = false;
+console.log ( esGato );
+console.log ( typeof esGato );
+
+let edad = 30;
+console.log ( edad );
+
+let superPoder;
+console.log (superPoder);
+console.log (typeof superPoder);
+
+let soyNull = null;
+console.log (typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log (typeof symbol1);
+console.log (symbol1 === symbol2); // false, cada símbolo es único  
