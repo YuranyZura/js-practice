194fe0397765ad3ec6851fd34c3827fe740c216e
[Yurany] <yuranysanchezmar18@gmail.com>
2026-03-10 14:36:53 -0500

    Palabras reservadas

---
+++ Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,16 @@
+
+
+//Palabra reservadas --- palabras para usos especificos
+
+let away = 123;
+
+// No usar carcateres especiales, por que las aplicacione s van a trabajar en ambientes de diferentes caracteres
+
+// let objeto123.123 = 123;   Esto quiere decir que se esta accediendo a la propiedad del objeto
+
+let jugadorConPuntajeMasAlto = "Messi"; //LowelcameloCase 
+
+class JuegoAnio {
+
+}
+
